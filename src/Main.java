b0884f23e4acad8e3e0f16e542f096a979555dc7
Lenovo <kontumer16@gmail.com>
2025-03-25 13:34:00 +0300

    все задания

--- src/Main.java
+++ src/Main.java
@@ -12,77 +12,44 @@ public class Main {
 
         // Задача 2:
         int clientDeviceYear = 2014;
-        if (clientOS == 0) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке.");
-            }
-        } else if (clientOS == 1) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке.");
-            }
-        } else {
+        String osType = clientOS == 0 ? "iOS" : clientOS == 1 ? "Android" : "неизвестная ОС";
+
+        if (osType.equals("неизвестная ОС")) {
             System.out.println("Операционная система не распознана.");
+        } else {
+            String versionType = clientDeviceYear < 2015 ? "облегченную" : "";
+            System.out.printf("Установите %sверсию приложения для %s по ссылке.%n",
+                    versionType, osType);
         }
 
         // Задача 3:
         int year = 2024;
-        if (year > 1584) {
-            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-                System.out.println(year + " год является високосным.");
-            } else {
-                System.out.println(year + " год не является високосным.");
-            }
-        } else {
+        if (year <= 1584) {
             System.out.println("Год должен быть больше 1584.");
+        } else {
+            boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+            System.out.println(year + " год " + (isLeap ? "является" : "не является") + " високосным.");
         }
 
         // Задача 4:
         int deliveryDistance = 95;
-        int deliveryTime = 1;
-
-        if (deliveryDistance > 20) {
-            deliveryTime++;
-        }
-        if (deliveryDistance > 60) {
-            deliveryTime++;
-        }
         if (deliveryDistance > 100) {
             System.out.println("Доставка свыше 100 км невозможна.");
         } else {
+            int deliveryTime = 1;
+            if (deliveryDistance > 60) deliveryTime += 2;
+            else if (deliveryDistance > 20) deliveryTime += 1;
             System.out.println("Потребуется дней: " + deliveryTime);
         }
 
         // Задача 5:
         int monthNumber = 12;
-        if (monthNumber >= 1 && monthNumber <= 12) {
-            switch (monthNumber) {
-                case 12:
-                case 1:
-                case 2:
-                    System.out.println("Месяц " + monthNumber + " принадлежит к сезону зима.");
-                    break;
-                case 3:
-                case 4:
-                case 5:
-                    System.out.println("Месяц " + monthNumber + " принадлежит к сезону весна.");
-                    break;
-                case 6:
-                case 7:
-                case 8:
-                    System.out.println("Месяц " + monthNumber + " принадлежит к сезону лето.");
-                    break;
-                case 9:
-                case 10:
-                case 11:
-                    System.out.println("Месяц " + monthNumber + " принадлежит к сезону осень.");
-                    break;
-            }
-        } else {
-            System.out.println("Номер месяца должен быть от 1 до 12.");
+        switch (monthNumber) {
+            case 12, 1, 2 -> System.out.println("Месяц " + monthNumber + " принадлежит к сезону зима.");
+            case 3, 4, 5 -> System.out.println("Месяц " + monthNumber + " принадлежит к сезону весна.");
+            case 6, 7, 8 -> System.out.println("Месяц " + monthNumber + " принадлежит к сезону лето.");
+            case 9, 10, 11 -> System.out.println("Месяц " + monthNumber + " принадлежит к сезону осень.");
+            default -> System.out.println("Номер месяца должен быть от 1 до 12.");
         }
     }
 }
